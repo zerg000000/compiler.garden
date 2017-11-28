2d33f419dd537481e8e7aa0e3da09f894b79077f
Albert Lai <albert.lai@algotizer.com>
2017-11-28 10:28:26 +0800

    update project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject compiler.garden "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Integrant methods for compiling Garden into CSS"
+  :url "https://github.com/zerg000000/compiler.garden"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-RC2"]
