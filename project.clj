ae3a186924f5d7c9a3d4ec59d219fa6b08ea0681
Albert Lai albert.lai@algotizer.com <albert.lai@algotizer.com>
2017-11-28 13:12:13 +0800

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compiler.garden "0.1.1"
+(defproject compiler.garden "0.1.2-SNAPSHOT"
   :description "Integrant methods for compiling Garden into CSS"
   :url "https://github.com/zerg000000/compiler.garden"
   :license {:name "Eclipse Public License"
